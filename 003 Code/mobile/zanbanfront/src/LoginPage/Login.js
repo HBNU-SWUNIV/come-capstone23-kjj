595285a8fd2200f815f8b23d9fba6f4a8975e40d
SeongHoon Jo <tjd0544@naver.com>
2023-04-21 15:19:03 +0900

    web

---
+++ 003 Code/mobile/zanbanfront/src/LoginPage/Login.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Login = () => {
+    return <h1>로그인 화면11</h1>
+}
+
+export default Login;
---
+++ 003 Code/mobile/zanbanfront/src/MyPage/My.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const My = () => {
+    return <h1>마이 화면</h1>
+}
+
+export default My;
