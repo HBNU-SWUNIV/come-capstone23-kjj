3a4e4ee13b8a6fffa48fc3ebbb5640832b8133cb
HyeongMokJeong <jhm1062@naver.com>
2023-11-27 23:41:27 +0900

    chore: fix properties

---
+++ 003Code/Backend/batch/src/main/java/com/batch/batch/config/JasyptConfig.java
@@ -0,0 +1,25 @@
+package com.batch.batch.config;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableEncryptableProperties
+public class JasyptConfig {
+
+    @Bean("jasypt")
+    public StringEncryptor stringEncryptor() {
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+
+        config.setPassword(System.getProperty("password"));
+        config.setAlgorithm("PBEWithMD5AndDES");
+        config.setPoolSize(1);
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
