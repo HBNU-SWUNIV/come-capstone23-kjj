797e5d7bee109b8f5229a00587f800c5f88a82c9
HyeongMokJeong <jhm1062@naver.com>
2023-12-07 22:20:18 +0900

    test: create Menu Controller test code

---
+++ 003Code/Backend/api/src/test/java/com/hanbat/zanbanzero/util/MenuTestTemplate.java
@@ -0,0 +1,38 @@
+package com.hanbat.zanbanzero.util;
+
+public class MenuTestTemplate {
+    public static Long testMenuId() {
+        return 1L;
+    }
+    public static Long testFoodId() {
+        return  1L; 
+    }
+    public static String testMenuName() {
+        return "test Menu name";
+    }
+    public static String testFoodName() {
+        return "test Food name";
+    }
+    public static int testCost() {
+        return 3000;
+    }
+    public static String testInfo() {
+        return "test info";
+    }
+    public static String testDetails() {
+        return "test details";
+    }
+    public static boolean testUsePlanner() {
+        return false;
+    }
+    public static String testImage() {
+        return "image";
+    }
+    public static boolean testSold() {
+        return true;
+    }
+    public static String testFilePath() {
+        return "img/menu";
+    }
+    public static String testFood() { return "test Food"; }
+}
